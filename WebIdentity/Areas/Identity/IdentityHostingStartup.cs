6897c0aeacf9635b1725b303ebfc2d867b144fa6
Brent Jacobs <brent.jacobs@zeiss.com>
2020-11-26 12:06:58 +1030

    Create full Identity UI source
    Folling directions:
    https://docs.microsoft.com/en-us/aspnet/core/security/authentication/scaffold-identity?view=aspnetcore-5.0&tabs=visual-studio#create-full-identity-ui-source

--- WebIdentity/Areas/Identity/IdentityHostingStartup.cs
+++ WebIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -2,11 +2,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using WebIdentity.Areas.Identity.Data;
 using WebIdentity.Data;
+using WebIdentity.Services;
 
 [assembly: HostingStartup(typeof(WebIdentity.Areas.Identity.IdentityHostingStartup))]
 namespace WebIdentity.Areas.Identity
@@ -20,8 +22,27 @@ namespace WebIdentity.Areas.Identity
                     options.UseSqlServer(
                         context.Configuration.GetConnectionString("IdentityDbContextConnection")));
 
-                services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
-                    .AddEntityFrameworkStores<IdentityDbContext>();
+                services
+                    .AddIdentity<User, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentityDbContext>()
+                    .AddDefaultTokenProviders();
+
+                services
+                    .AddMvc()
+                    .AddRazorPagesOptions(options =>
+                    {
+                        options.Conventions.AuthorizeAreaFolder("Identity", "/Account/Manage");
+                        options.Conventions.AuthorizeAreaPage("Identity", "/Account/Logout");
+                    });
+
+                services.ConfigureApplicationCookie(options =>
+                {
+                    options.LoginPath = $"/Identity/Account/Login";
+                    options.LogoutPath = $"/Identity/Account/Logout";
+                    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+                });
+
+                services.AddSingleton<IEmailSender, EmailSender>();
             });
         }
     }
---
+++ WebIdentity/Services/EmailSender.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebIdentity.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public ILogger<EmailSender> Logger { get; set; }
+
+        public EmailSender(ILogger<EmailSender> logger) => Logger = logger;
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            Logger.LogInformation("Sending Email (to: {email}, subject: {subject}, body: {htmlMessage})", email, subject, htmlMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
